075fb4c5694a346e03eb3ad42cddfeeec96926ef
nitely <ecastroborsani@gmail.com>
2024-05-27 15:17:43 -0300

    wip

--- examples/tls_client.nim
+++ examples/tls_client.nim
@@ -3,12 +3,9 @@
 import std/asyncdispatch
 import std/streams
 
-import pkg/hyperx/client
 import pkg/protobuf
 
-import ../src/grpc/errors
-import ../src/grpc/headers
-import ../src/grpc/types
+import ../src/grpc/client
 
 const protoDef = """
 syntax = "proto3";
@@ -22,10 +19,6 @@ message HelloReply {
 }
 """
 parseProto(protoDef)
-
-func newSeqRef[T](s: seq[T]): ref seq[T] =
-  result = new(seq[T])
-  result[] = s
 
 func newStringRef(s = ""): ref string =
   new result
@@ -66,80 +59,17 @@ proc decode(s: string): StringStream =
   result.write fromWireData(s)
   result.setPosition(0)
 
-proc recv(
-  strm: ClientStream,
-  headers, data: ref string
-) {.async.} =
-  await strm.recvHeaders(headers)
-  while not strm.recvEnded:
-    await strm.recvBody(data)
-
-proc send(
-  strm: ClientStream,
-  data: ref string
-) {.async.} =
-  await strm.sendHeaders(
-    newSeqRef(@[
-      (":method", "POST"),
-      (":scheme", "https"),
-      (":path", "/helloworld.Greeter/SayHello"),
-      (":authority", "localhost"),
-      ("te", "trailers"),
-      ("grpc-encoding", "identity"),
-      ("grpc-accept-encoding", "identity"),
-      ("user-agent", "grpc-nim/0.1.0"),
-      ("content-type", "application/grpc+proto"),
-      ("content-length", $data[].len)
-    ]),
-    finish = false
-  )
-  await strm.sendBody(data, finish = true)
-
 proc main() {.async.} =
   var client = newClient("127.0.0.1", Port 50051)
   withClient client:
-    let strm = client.newClientStream()
-    withStream strm:
-      # start recv before send in case of error
-      let headersIn = newStringRef()
-      let dataIn = newStringRef()
-      let recvFut = strm.recv(headersIn, dataIn)
-      let msg = new HelloRequest
-      msg.name = "you"
-      let dataOut = newStringRef(encode(msg))
-      let sendFut = strm.send(dataOut)
-      var connErr: ref HyperxConnError = nil
-      var strmErr: ref HyperxStrmError = nil
-      try:
-        await sendFut
-      except HyperxStrmError as err:
-        debugEcho err.msg
-        strmErr = err
-      except HyperxConnError as err:
-        debugEcho err.msg
-        connErr = err
-      try:
-        await recvFut
-      except HyperxStrmError as err:
-        debugEcho err.msg
-        strmErr = err
-      except HyperxConnError as err:
-        debugEcho err.msg
-        connErr = err
-      echo headersIn[]
-      let respHeaders = toResponseHeaders headersIn[]
-      if respHeaders.status != stcOk:
-        raise newGrpcResponseError(
-          respHeaders.statusMsg,
-          respHeaders.status
-        )
-      if connErr != nil:
-        raise (ref HyperxConnError)(msg: connErr.msg)
-      if strmErr != nil:
-        raise (ref HyperxStrmError)(msg: strmErr.msg)
-      if dataIn[].len > 0:
-        let readMsg = dataIn[].decode().readHelloRequest()
-        if readMsg.has(name):
-          echo readMsg.name
+    let msg = new HelloRequest
+    msg.name = "you"
+    let data = await client.get(
+      newStringRef("/helloworld.Greeter/SayHello"),
+      newStringRef(encode(msg))
+    )
+    let readMsg = data[].decode().readHelloRequest()
+    if readMsg.has(name):
+      echo readMsg.name
 
 waitFor main()
---
+++ src/grpc/client.nim
@@ -0,0 +1,95 @@
+
+import std/asyncdispatch
+import std/streams
+
+import pkg/hyperx/client
+
+import ./errors
+import ./headers
+import ./types
+
+export
+  ClientContext,
+  newClient,
+  withClient,
+  GrpcResponseError
+
+func newSeqRef[T](s: seq[T]): ref seq[T] =
+  result = new(seq[T])
+  result[] = s
+
+func newStringRef(s = ""): ref string =
+  new result
+  result[] = s
+
+proc recv(
+  strm: ClientStream,
+  headers, data: ref string
+) {.async.} =
+  await strm.recvHeaders(headers)
+  while not strm.recvEnded:
+    await strm.recvBody(data)
+
+proc send(
+  strm: ClientStream,
+  path: ref string,
+  data: ref string
+) {.async.} =
+  await strm.sendHeaders(
+    newSeqRef(@[
+      (":method", "POST"),
+      (":scheme", "https"),
+      (":path", path[]),
+      (":authority", strm.client.hostname),
+      ("te", "trailers"),
+      ("grpc-encoding", "identity"),
+      ("grpc-accept-encoding", "identity"),
+      ("user-agent", "grpc-nim/0.1.0"),
+      ("content-type", "application/grpc+proto"),
+      ("content-length", $data[].len)
+    ]),
+    finish = false
+  )
+  await strm.sendBody(data, finish = true)
+
+proc get*(
+  client: ClientContext,
+  path: ref string,
+  data: ref string
+): Future[ref string] {.async.} =
+  let strm = client.newClientStream()
+  withStream strm:
+    # start recv before send in case of error
+    result = newStringRef()
+    let headersIn = newStringRef()
+    let recvFut = strm.recv(headersIn, result)
+    let sendFut = strm.send(path, data)
+    var connErr: ref HyperxConnError = nil
+    var strmErr: ref HyperxStrmError = nil
+    try:
+      await sendFut
+    except HyperxStrmError as err:
+      debugEcho err.msg
+      strmErr = err
+    except HyperxConnError as err:
+      debugEcho err.msg
+      connErr = err
+    try:
+      await recvFut
+    except HyperxStrmError as err:
+      debugEcho err.msg
+      strmErr = err
+    except HyperxConnError as err:
+      debugEcho err.msg
+      connErr = err
+    #echo headersIn[]
+    let respHeaders = toResponseHeaders headersIn[]
+    if respHeaders.status != stcOk:
+      raise newGrpcResponseError(
+        respHeaders.statusMsg,
+        respHeaders.status
+      )
+    if connErr != nil:
+      raise (ref HyperxConnError)(msg: connErr.msg)
+    if strmErr != nil:
+      raise (ref HyperxStrmError)(msg: strmErr.msg)
