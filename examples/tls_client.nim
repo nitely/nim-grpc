597ba8b45482b9335c2346cb2fffb9c1090d1fcb
nitely <ecastroborsani@gmail.com>
2024-06-18 21:12:23 -0300

    wip

--- examples/tls_client.nim
+++ examples/tls_client.nim
@@ -6,6 +6,7 @@ import std/streams
 import pkg/protobuf
 
 import ../src/grpc/client
+import ../src/grpc/utils
 
 const protoDef = """
 syntax = "proto3";
@@ -19,45 +20,6 @@ message HelloReply {
 }
 """
 parseProto(protoDef)
-
-func newStringRef(s = ""): ref string =
-  new result
-  result[] = s
-
-func add(s: var string, ss: openArray[char]) {.raises: [].} =
-  let L = s.len
-  s.setLen(L+ss.len)
-  for i in 0 .. ss.len-1:
-    s[L+i] = ss[i]
-
-func toWireData(msg: string): string =
-  template ones(n: untyped): uint = (1.uint shl n) - 1
-  let L = msg.len.uint
-  result = newStringOfCap(msg.len+5)
-  result.setLen 5
-  result[0] = 0.char  # uncompressed
-  result[1] = ((L shr 24) and 8.ones).char
-  result[2] = ((L shr 16) and 8.ones).char
-  result[3] = ((L shr 8) and 8.ones).char
-  result[4] = (L and 8.ones).char
-  result.add msg
-
-proc encode[T](s: T): string =
-  var ss = newStringStream()
-  ss.write s
-  ss.setPosition 0
-  result = ss.readAll.toWireData
-
-func fromWireData(data: string): string =
-  doAssert data.len >= 5
-  doAssert data[0] == 0.char  # XXX uncompress
-  result = newStringOfCap(data.len-5)
-  result.add toOpenArray(data, 5, data.len-1)
-
-proc decode(s: string): StringStream =
-  result = newStringStream()
-  result.write fromWireData(s)
-  result.setPosition(0)
 
 proc sayHello(
   client: ClientContext, request: HelloRequest
@@ -93,7 +55,8 @@ proc helloReply(strm: GrpcStream): Future[HelloReply] {.async.} =
   result = data[].decode().readHelloReply()
 
 proc main() {.async.} =
-  var client = newClient("127.0.0.1", Port 50051)
+  #var client = newClient("127.0.0.1", Port 50051)
+  var client = newClient("127.0.0.1", Port 4443)
   withClient client:
     block:
       echo "Simple request"
@@ -102,7 +65,7 @@ proc main() {.async.} =
       let reply = await client.sayHello(request)
       if reply.has(message):
         echo reply.message
-    block:
+    when false:
       echo "Stream reply"
       let stream = client.sayHelloStreamReply()
       with stream:
@@ -113,7 +76,7 @@ proc main() {.async.} =
           let reply = await stream.helloReply()
           if reply.has(message):
             echo reply.message
-    block:
+    when false:
       echo "Bidirectional stream"
       let stream = client.sayHelloBidiStream()
       with stream:
---
+++ src/grpc/utils.nim
@@ -0,0 +1,40 @@
+
+import std/streams
+import std/strbasics
+
+func newStringRef*(s = ""): ref string =
+  new result
+  result[] = s
+
+func newSeqRef*[T](s: seq[T] = @[]): ref seq[T] =
+  new result
+  result[] = s
+
+func toWireData*(msg: string): string =
+  template ones(n: untyped): uint = (1.uint shl n) - 1
+  let L = msg.len.uint
+  result = newStringOfCap(msg.len+5)
+  result.setLen 5
+  result[0] = 0.char  # uncompressed
+  result[1] = ((L shr 24) and 8.ones).char
+  result[2] = ((L shr 16) and 8.ones).char
+  result[3] = ((L shr 8) and 8.ones).char
+  result[4] = (L and 8.ones).char
+  result.add msg
+
+proc encode*[T](s: T): string =
+  var ss = newStringStream()
+  ss.write s
+  ss.setPosition 0
+  result = ss.readAll.toWireData
+
+func fromWireData*(data: string): string =
+  doAssert data.len >= 5
+  doAssert data[0] == 0.char  # XXX uncompress
+  result = newStringOfCap(data.len-5)
+  result.add toOpenArray(data, 5, data.len-1)
+
+proc decode*(s: string): StringStream =
+  result = newStringStream()
+  result.write fromWireData(s)
+  result.setPosition(0)
